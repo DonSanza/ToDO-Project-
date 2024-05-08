6e41858ffe95eba8746f71c5261798b77a4d8389
Sandile Mazibuko <133252205+DonSanza@users.noreply.github.com>
2024-05-08 16:21:36 +0200

    Add files via upload

---
+++ todoproject/src/main/java/com/todoapp/todoproject/Entity/User.java
@@ -0,0 +1,49 @@
+package com.todoapp.todoproject.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(name = "username", unique = true)
+    private String username;
+    private String password;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
